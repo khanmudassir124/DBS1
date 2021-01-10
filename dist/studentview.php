9b66f15a5bbd80b40e01ad7007068acf06815d8f
Mudassir <khanmudassir124@gmail.com>
2021-01-10 19:05:58 +0530

    Initial Commit

---
+++ dist/studentview.php
@@ -0,0 +1,116 @@
+<?php include('nav.php');?>
+
+            <div id="layoutSidenav_content">
+                <main>
+                    <div class="container-fluid">
+                        <h1 class="mt-4">Student Section</h1>
+                        <ol class="breadcrumb mb-4">
+                            <li class="breadcrumb-item"><a href="index.html">Dashboard</a></li>
+                            <li class="breadcrumb-item active">Student Database</li>
+                        </ol>
+                        <!-- <div class="card mb-4">
+                            <div class="card-body">
+                                <p class="mb-0">
+                                    This page is an example of using static navigation. By removing the
+                                    <code>.sb-nav-fixed</code>
+                                    class from the
+                                    <code>body</code>
+                                    , the top navigation and side navigation will become static on scroll. Scroll down this page to see an example.
+                                </p>
+                            </div>
+                        </div> -->
+                        <div>
+                            <div class="row">
+                            <div class="col-md-6">
+                                        <label class="small mb-0" for="inputFirstName"><strong>First Name</strong></label>
+                                        <p class="" id="inputFirstName">Samay</p>
+                                </div>
+                                <div class="col-md-6">
+                                        <label class="small mb-0" for="inputLastName"><strong>Last Name</strong></label>
+                                        <p class="" id="inputLastName">Raina</p>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-md-6">
+                                        <label class="small mb-0" for="stfathername"><strong>Fathers Name</strong></label>
+                                        <p class="" id="stfathername">Ajay Raina</p>
+                                </div>
+                                <div class="col-md-6">
+                                        <label class="small mb-0" for="stmothername"><strong>Mothers Name</strong></label>
+                                        <p class="" id="stmothername">Sonia Raina</p>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-md-4">
+                                        <label class="small mb-0" for="stdob"><strong>Date Of Birth</strong></label>
+                                        <p class="" id="stdob">22th May,2014</p>
+                                </div>
+                                <div class="col-md-4">
+                                        <label class="small mb-0" for="stclass"><strong>Class</strong></label>
+                                        <p class="" id="stclass">Second II</p>
+                                </div>
+                                <div class="col-md-4">
+                                        <label class="small mb-0" for="stgender"><strong>Gender</strong></label>
+                                        <p class="" id="stgender">Mail</p>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-md-6">
+                                        <label class="small mb-0" for="inputEmailAddress"><strong>Email</strong></label>
+                                        <p class="" id="inputEmailAddress">abc@gmail.com</p>
+                                </div>
+                                <div class="col-md-6">
+                                        <label class="small mb-0" for="staadhaar"><strong>Adhaar No.</strong></label>
+                                        <p class="" id="staadhaar">702730997992030</p>
+                                </div>
+                            </div>            
+                                    <!-- <div class="form-row">
+                                <div class="col-md-6">
+                                    <div class="form-group">
+                                        <label class="small mb-1" for="inputPassword">Password</label>
+                                        <input class="form-control py-4" id="inputPassword" type="password" placeholder="Enter password" />
+                                    </div>
+                                </div>
+                                <div class="col-md-6">
+                                    <div class="form-group">
+                                        <label class="small mb-1" for="inputConfirmPassword">Confirm Password</label>
+                                        <input class="form-control py-4" id="inputConfirmPassword" type="password" placeholder="Confirm password" />
+                                    </div>
+                                </div>
+                            </div> -->
+                            <div class="row">
+                                <div class="col-md-6">
+                                        <label class="small mb-0" for="stcontact"><strong>Mobile No.</strong></label>
+                                        <p class="" id="stcontact">7027309979</p>
+                                </div>
+                                <div class="col-md-6">
+                                        <label class="small mb-0" for="ptcontact"><strong>Emergency Contact No.</strong></label>
+                                        <p class="" id="ptcontact">9872635482</p>
+                                </div>
+                            </div>
+                                <label class="small mb-0" for="staddress"><strong>Address</strong></label>
+                                <p class="" id="staddress">With supporting text below as a natural lead-in to additional content.</p>
+                            <button class="btn btn-primary" type="submit">Edit</button>
+                        </div>  
+                        <!-- <div class="card mb-4"><div class="card-body">When scrolling, the navigation stays at the top of the page. This is the end of the static navigation demo.</div></div> -->
+                    </div>
+                </main>
+                <footer class="py-4 bg-light mt-auto">
+                    <div class="container-fluid">
+                        <div class="d-flex align-items-center justify-content-between small">
+                            <div class="text-muted">Copyright &copy; Your Website 2020</div>
+                            <div>
+                                <a href="#">Privacy Policy</a>
+                                &middot;
+                                <a href="#">Terms &amp; Conditions</a>
+                            </div>
+                        </div>
+                    </div>
+                </footer>
+            </div>
+        </div>
+        <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
